6719de005e0c507a85bb7272410c115bebcf4cda
zhaochenjun <holyoops@hotmail.com>
2016-03-18 17:21:51 +0800

    WEIXIN KEY

--- CountMeIn/Util/Common.swift
+++ CountMeIn/Util/Common.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+// WAO
+//let weChatAppid = "wx74587b9207ae1ef6"
+//let weChatSecret = "acd901b1dc3bcba6ed1e4bbe50b91d73"
 
-let weChatAppid = "wx74587b9207ae1ef6"
-let weChatSecret = "acd901b1dc3bcba6ed1e4bbe50b91d73"
+// ZHAOCHENJUN
+//let weChatAppid = "wx11f39a75d23f49f0"
+//let weChatSecret = "cafd39507178263493418a720a8a7ab0"
+
+// COUNT ME IN
+let weChatAppid = "wxb74391e462f751a0"
+let weChatSecret = "e08f3ecbcfacc042dc49f5fb576393e7"
+
+// ZIJINWANG
+//let weChatAppid = "wx967f65b9e6652798"
+//let weChatSecret = "08dc3f4bbf2b7fe28e4cf8b7042a520c"
 
 let URL_WECHAT_ACCESSTOKEN = "https://api.weixin.qq.com/sns/oauth2/access_token"
 let URL_WECHAT_USERINFO = "https://api.weixin.qq.com/sns/userinfo"
